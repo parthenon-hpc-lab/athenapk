18b47f8b4a2d761f517894d1312c28629a89b6b3
Philipp Grete <gretephi@msu.edu>
2020-12-30 13:42:13 +0100

    Reintroduce AMR

--- src/hydro/hydro_driver.cpp
+++ src/hydro/hydro_driver.cpp
@@ -14,6 +14,7 @@
 #include "bvals/cc/bvals_cc_in_one.hpp"
 #include "interface/update.hpp"
 #include "parthenon/driver.hpp"
+#include "refinement/refinement.hpp"
 #include "tasks/task_id.hpp"
 #include "utils/partition_stl_containers.hpp"
 // Athena headers
@@ -34,21 +35,6 @@ HydroDriver::HydroDriver(ParameterInput *pin, ApplicationInput *app_in, Mesh *pm
   pin->CheckDesired("hydro", "cfl");
 }
 
-// first some helper tasks
-auto UpdateContainer(const int stage, Integrator *integrator,
-                     std::shared_ptr<parthenon::MeshData<Real>> &base,
-                     std::shared_ptr<parthenon::MeshData<Real>> &dudt,
-                     std::shared_ptr<parthenon::MeshData<Real>> &out) -> TaskStatus {
-  // TODO(pgrete): this update is currently hardcoded to work for rk1 and vl2
-  const Real beta = integrator->beta[stage - 1];
-  const Real dt = integrator->dt;
-
-  parthenon::Update::UpdateIndependentData<MeshData<Real>>(base.get(), dudt.get(),
-                                                           beta * dt, out.get());
-
-  return TaskStatus::complete;
-}
-
 // See the advection.hpp declaration for a description of how this function gets called.
 TaskCollection HydroDriver::MakeTaskCollection(BlockList_t &blocks, int stage) {
   TaskCollection tc;
@@ -59,11 +45,11 @@ TaskCollection HydroDriver::MakeTaskCollection(BlockList_t &blocks, int stage) {
   // Being extra verbose here in this example to highlight that this is not
   // required to be 1 or blocks.size() but could also only apply to a subset of blocks.
   auto num_task_lists_executed_independently = blocks.size();
-  TaskRegion &async_region1 = tc.AddRegion(num_task_lists_executed_independently);
 
+  TaskRegion &async_region_1 = tc.AddRegion(num_task_lists_executed_independently);
   for (int i = 0; i < blocks.size(); i++) {
     auto &pmb = blocks[i];
-    auto &tl = async_region1[i];
+    auto &tl = async_region_1[i];
     // first make other useful containers
     if (stage == 1) {
       auto &base = pmb->meshblock_data.Get();
@@ -85,56 +71,83 @@ TaskCollection HydroDriver::MakeTaskCollection(BlockList_t &blocks, int stage) {
                                  BoundaryCommSubset::all);
   }
 
-  const auto &eos = blocks[0]->packages["Hydro"]->Param<AdiabaticHydroEOS>("eos");
-  const auto &pack_in_one = blocks[0]->packages["Hydro"]->Param<bool>("pack_in_one");
-  const auto &use_scratch = blocks[0]->packages["Hydro"]->Param<bool>("use_scratch");
   const int num_partitions = pmesh->DefaultNumPartitions();
   // note that task within this region that contains one tasklist per pack
   // could still be executed in parallel
   TaskRegion &single_tasklist_per_pack_region = tc.AddRegion(num_partitions);
   for (int i = 0; i < num_partitions; i++) {
     auto &tl = single_tasklist_per_pack_region[i];
-    auto &mbase = pmesh->mesh_data.GetOrAdd("base", i);
     auto &mc0 = pmesh->mesh_data.GetOrAdd(stage_name[stage - 1], i);
-    auto &mc1 = pmesh->mesh_data.GetOrAdd(stage_name[stage], i);
-    auto &mdudt = pmesh->mesh_data.GetOrAdd("dUdt", i);
 
     TaskID advect_flux;
+    const auto &use_scratch = blocks[0]->packages["Hydro"]->Param<bool>("use_scratch");
+    const auto &eos = blocks[0]->packages["Hydro"]->Param<AdiabaticHydroEOS>("eos");
     if (use_scratch) {
       advect_flux = tl.AddTask(none, Hydro::CalculateFluxesWScratch, mc0, stage);
     } else {
       advect_flux = tl.AddTask(none, Hydro::CalculateFluxes, stage, mc0, eos);
     }
+  }
+  TaskRegion &async_region_2 = tc.AddRegion(num_task_lists_executed_independently);
+  for (int i = 0; i < blocks.size(); i++) {
+    auto &tl = async_region_2[i];
+    auto &sc0 = blocks[i]->meshblock_data.Get(stage_name[stage - 1]);
+    auto send_flux =
+        tl.AddTask(none, &MeshBlockData<Real>::SendFluxCorrection, sc0.get());
+    auto recv_flux =
+        tl.AddTask(none, &MeshBlockData<Real>::ReceiveFluxCorrection, sc0.get());
+  }
+
+  TaskRegion &single_tasklist_per_pack_region_2 = tc.AddRegion(num_partitions);
+  for (int i = 0; i < num_partitions; i++) {
+    auto &tl = single_tasklist_per_pack_region_2[i];
+
+    auto &mbase = pmesh->mesh_data.GetOrAdd("base", i);
+    auto &mc0 = pmesh->mesh_data.GetOrAdd(stage_name[stage - 1], i);
+    auto &mc1 = pmesh->mesh_data.GetOrAdd(stage_name[stage], i);
+    auto &mdudt = pmesh->mesh_data.GetOrAdd("dUdt", i);
 
     // compute the divergence of fluxes of conserved variables
-    auto flux_div =
-        tl.AddTask(advect_flux, parthenon::Update::FluxDivergence<MeshData<Real>>,
-                   mc0.get(), mdudt.get());
+    auto flux_div = tl.AddTask(none, parthenon::Update::FluxDivergence<MeshData<Real>>,
+                               mc0.get(), mdudt.get());
 
     // apply du/dt to all independent fields in the container
-    auto update_container =
-        tl.AddTask(flux_div, UpdateContainer, stage, integrator, mbase, mdudt, mc1);
+    // TODO(pgrete): this update is currently hardcoded to work for rk1 and vl2
+    //               need to reintroduce proper stages and averaging to  make it work
+    //               with other integrators again.
+    const Real beta = integrator->beta[stage - 1];
+    const Real dt = integrator->dt;
+    auto update =
+        tl.AddTask(flux_div, parthenon::Update::UpdateIndependentData<MeshData<Real>>,
+                   mbase.get(), mdudt.get(), beta * dt, mc1.get());
 
     // update ghost cells
-    auto send = tl.AddTask(update_container,
-                           parthenon::cell_centered_bvars::SendBoundaryBuffers, mc1);
+    auto send =
+        tl.AddTask(update, parthenon::cell_centered_bvars::SendBoundaryBuffers, mc1);
     auto recv =
         tl.AddTask(send, parthenon::cell_centered_bvars::ReceiveBoundaryBuffers, mc1);
     auto fill_from_bufs =
         tl.AddTask(recv, parthenon::cell_centered_bvars::SetBoundaries, mc1);
   }
 
-  TaskRegion &async_region3 = tc.AddRegion(num_task_lists_executed_independently);
+  TaskRegion &async_region_3 = tc.AddRegion(num_task_lists_executed_independently);
   for (int i = 0; i < blocks.size(); i++) {
-    auto &pmb = blocks[i];
-    auto &tl = async_region3[i];
-    auto &sc1 = pmb->meshblock_data.Get(stage_name[stage]);
+    auto &tl = async_region_3[i];
+    auto &sc1 = blocks[i]->meshblock_data.Get(stage_name[stage]);
     auto clear_comm_flags = tl.AddTask(none, &MeshBlockData<Real>::ClearBoundary,
                                        sc1.get(), BoundaryCommSubset::all);
+    auto prolongBound = none;
+    if (pmesh->multilevel) {
+      prolongBound = tl.AddTask(none, parthenon::ProlongateBoundaries, sc1);
+    }
+
+    // set physical boundaries
+    auto set_bc = tl.AddTask(prolongBound, parthenon::ApplyBoundaryConditions, sc1);
   }
-  TaskRegion &single_tasklist_per_pack_region2 = tc.AddRegion(num_partitions);
+
+  TaskRegion &single_tasklist_per_pack_region_3 = tc.AddRegion(num_partitions);
   for (int i = 0; i < num_partitions; i++) {
-    auto &tl = single_tasklist_per_pack_region2[i];
+    auto &tl = single_tasklist_per_pack_region_3[i];
     auto &mc1 = pmesh->mesh_data.GetOrAdd(stage_name[stage], i);
     auto fill_derived =
         tl.AddTask(none, parthenon::Update::FillDerived<MeshData<Real>>, mc1.get());
@@ -142,6 +155,16 @@ TaskCollection HydroDriver::MakeTaskCollection(BlockList_t &blocks, int stage) {
     if (stage == integrator->nstages) {
       auto new_dt = tl.AddTask(
           fill_derived, parthenon::Update::EstimateTimestep<MeshData<Real>>, mc1.get());
+    }
+  }
+
+  if (stage == integrator->nstages && pmesh->adaptive) {
+    TaskRegion &async_region_4 = tc.AddRegion(num_task_lists_executed_independently);
+    for (int i = 0; i < blocks.size(); i++) {
+      auto &tl = async_region_4[i];
+      auto &sc1 = blocks[i]->meshblock_data.Get(stage_name[stage]);
+      auto tag_refine =
+          tl.AddTask(none, parthenon::Refinement::Tag<MeshBlockData<Real>>, sc1.get());
     }
   }
 
