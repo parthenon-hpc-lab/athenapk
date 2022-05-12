258905f9baadef3e1cca6f977fff79dac1ee2b43
Philipp Grete <gretephi@msu.edu>
2022-05-12 11:20:58 +0200

    Update hllc solver to match AthenaPK interfaces

--- src/hydro/rsolvers/hydro_hllc.hpp
+++ src/hydro/rsolvers/hydro_hllc.hpp
@@ -13,113 +13,91 @@
 //! - P. Batten, N. Clarke, C. Lambert, and D. M. Causon, "On the Choice of Wavespeeds
 //!   for the HLLC Riemann Solver", SIAM J. Sci. & Stat. Comp. 18, 6, 1553-1570, (1997).
 
-// C headers
+#ifndef RSOLVERS_HYDRO_HLLC_HPP_
+#define RSOLVERS_HYDRO_HLLC_HPP_
 
 // C++ headers
 #include <algorithm> // max(), min()
 #include <cmath>     // sqrt()
 
-// Athena++ headers
-#include "../../../athena.hpp"
-#include "../../../athena_arrays.hpp"
-#include "../../../eos/eos.hpp"
-#include "../../hydro.hpp"
+// AthenaPK headers
+#include "../../main.hpp"
+#include "rsolvers.hpp"
 
 //----------------------------------------------------------------------------------------
 //! \fn void Hydro::RiemannSolver
 //! \brief The HLLC Riemann solver for adiabatic hydrodynamics (use HLLE for isothermal)
 
-void Hydro::RiemannSolver(const int k, const int j, const int il, const int iu,
-                          const int ivx, AthenaArray<Real> &wl, AthenaArray<Real> &wr,
-                          AthenaArray<Real> &flx, const AthenaArray<Real> &dxw) {
-  int ivy = IVX + ((ivx - IVX) + 1) % 3;
-  int ivz = IVX + ((ivx - IVX) + 2) % 3;
-  Real wli[(NHYDRO)], wri[(NHYDRO)];
-  Real flxi[(NHYDRO)], fl[(NHYDRO)], fr[(NHYDRO)];
-  Real gamma;
-  if (GENERAL_EOS) {
-    gamma = std::nan("");
-  } else {
-    gamma = pmy_block->peos->GetGamma();
-  }
+template <>
+struct Riemann<Fluid::euler, RiemannSolver::hllc> {
+  static KOKKOS_INLINE_FUNCTION void
+  Solve(parthenon::team_mbr_t const &member, const int k, const int j, const int il,
+        const int iu, const int ivx, const ScratchPad2D<Real> &wl,
+        const ScratchPad2D<Real> &wr, VariableFluxPack<Real> &cons,
+        const AdiabaticHydroEOS &eos, const Real c_h) {
+  int ivy = IV1 + ((ivx - IV1) + 1) % 3;
+  int ivz = IV1 + ((ivx - IV1) + 2) % 3;
+  Real gamma = eos.GetGamma();
   Real gm1 = gamma - 1.0;
   Real igm1 = 1.0 / gm1;
 
-#pragma omp simd private(wli, wri, flxi, fl, fr)
-#pragma distribute_point
-  for (int i = il; i <= iu; ++i) {
+  parthenon::par_for_inner(member, il, iu, [&](const int i) {
+    Real wli[(NHYDRO)], wri[(NHYDRO)];
+    Real fl[(NHYDRO)], fr[(NHYDRO)], flxi[(NHYDRO)];
     //--- Step 1.  Load L/R states into local variables
     wli[IDN] = wl(IDN, i);
-    wli[IVX] = wl(ivx, i);
-    wli[IVY] = wl(ivy, i);
-    wli[IVZ] = wl(ivz, i);
+    wli[IV1] = wl(ivx, i);
+    wli[IV2] = wl(ivy, i);
+    wli[IV3] = wl(ivz, i);
     wli[IPR] = wl(IPR, i);
 
     wri[IDN] = wr(IDN, i);
-    wri[IVX] = wr(ivx, i);
-    wri[IVY] = wr(ivy, i);
-    wri[IVZ] = wr(ivz, i);
+    wri[IV1] = wr(ivx, i);
+    wri[IV2] = wr(ivy, i);
+    wri[IV3] = wr(ivz, i);
     wri[IPR] = wr(IPR, i);
 
     //--- Step 2.  Compute middle state estimates with PVRS (Toro 10.5.2)
 
     Real al, ar, el, er;
-    Real cl = pmy_block->peos->SoundSpeed(wli);
-    Real cr = pmy_block->peos->SoundSpeed(wri);
-    if (GENERAL_EOS) {
-      el = pmy_block->peos->EgasFromRhoP(wli[IDN], wli[IPR]) +
-           0.5 * wli[IDN] * (SQR(wli[IVX]) + SQR(wli[IVY]) + SQR(wli[IVZ]));
-      er = pmy_block->peos->EgasFromRhoP(wri[IDN], wri[IPR]) +
-           0.5 * wri[IDN] * (SQR(wri[IVX]) + SQR(wri[IVY]) + SQR(wri[IVZ]));
-    } else {
+    Real cl = eos.SoundSpeed(wli);
+    Real cr = eos.SoundSpeed(wri);
       el = wli[IPR] * igm1 +
-           0.5 * wli[IDN] * (SQR(wli[IVX]) + SQR(wli[IVY]) + SQR(wli[IVZ]));
+           0.5 * wli[IDN] * (SQR(wli[IV1]) + SQR(wli[IV2]) + SQR(wli[IV3]));
       er = wri[IPR] * igm1 +
-           0.5 * wri[IDN] * (SQR(wri[IVX]) + SQR(wri[IVY]) + SQR(wri[IVZ]));
-    }
+           0.5 * wri[IDN] * (SQR(wri[IV1]) + SQR(wri[IV2]) + SQR(wri[IV3]));
     Real rhoa = .5 * (wli[IDN] + wri[IDN]); // average density
     Real ca = .5 * (cl + cr);               // average sound speed
-    Real pmid = .5 * (wli[IPR] + wri[IPR] + (wli[IVX] - wri[IVX]) * rhoa * ca);
-    Real umid = .5 * (wli[IVX] + wri[IVX] + (wli[IPR] - wri[IPR]) / (rhoa * ca));
-    Real rhol = wli[IDN] + (wli[IVX] - umid) * rhoa / ca; // mid-left density
-    Real rhor = wri[IDN] + (umid - wri[IVX]) * rhoa / ca; // mid-right density
+    Real pmid = .5 * (wli[IPR] + wri[IPR] + (wli[IV1] - wri[IV1]) * rhoa * ca);
+    Real umid = .5 * (wli[IV1] + wri[IV1] + (wli[IPR] - wri[IPR]) / (rhoa * ca));
+    Real rhol = wli[IDN] + (wli[IV1] - umid) * rhoa / ca; // mid-left density
+    Real rhor = wri[IDN] + (umid - wri[IV1]) * rhoa / ca; // mid-right density
 
     //--- Step 3.  Compute sound speed in L,R
 
     Real ql, qr;
-    if (GENERAL_EOS) {
-      Real gl = pmy_block->peos->AsqFromRhoP(rhol, pmid) * rhol / pmid;
-      Real gr = pmy_block->peos->AsqFromRhoP(rhor, pmid) * rhor / pmid;
-      ql = (pmid <= wli[IPR])
-               ? 1.0
-               : std::sqrt(1.0 + (gl + 1) / (2 * gl) * (pmid / wli[IPR] - 1.0));
-      qr = (pmid <= wri[IPR])
-               ? 1.0
-               : std::sqrt(1.0 + (gr + 1) / (2 * gr) * (pmid / wri[IPR] - 1.0));
-    } else {
       ql = (pmid <= wli[IPR])
                ? 1.0
                : std::sqrt(1.0 + (gamma + 1) / (2 * gamma) * (pmid / wli[IPR] - 1.0));
       qr = (pmid <= wri[IPR])
                ? 1.0
                : std::sqrt(1.0 + (gamma + 1) / (2 * gamma) * (pmid / wri[IPR] - 1.0));
-    }
 
     //--- Step 4.  Compute the max/min wave speeds based on L/R
 
-    al = wli[IVX] - cl * ql;
-    ar = wri[IVX] + cr * qr;
+    al = wli[IV1] - cl * ql;
+    ar = wri[IV1] + cr * qr;
 
     Real bp = ar > 0.0 ? ar : (TINY_NUMBER);
     Real bm = al < 0.0 ? al : -(TINY_NUMBER);
 
     //--- Step 5. Compute the contact wave speed and pressure
 
-    Real vxl = wli[IVX] - al;
-    Real vxr = wri[IVX] - ar;
+    Real vxl = wli[IV1] - al;
+    Real vxr = wri[IV1] - ar;
 
-    Real tl = wli[IPR] + vxl * wli[IDN] * wli[IVX];
-    Real tr = wri[IPR] + vxr * wri[IDN] * wri[IVX];
+    Real tl = wli[IPR] + vxl * wli[IDN] * wli[IV1];
+    Real tr = wri[IPR] + vxr * wri[IDN] * wri[IV1];
 
     Real ml = wli[IDN] * vxl;
     Real mr = -(wri[IDN] * vxr);
@@ -130,27 +108,25 @@ void Hydro::RiemannSolver(const int k, const int j, const int il, const int iu,
     Real cp = (ml * tr + mr * tl) / (ml + mr);
     cp = cp > 0.0 ? cp : 0.0;
 
-    // No loop-carried dependencies anywhere in this loop
-    //    #pragma distribute_point
     //--- Step 6. Compute L/R fluxes along the line bm, bp
 
-    vxl = wli[IVX] - bm;
-    vxr = wri[IVX] - bp;
+    vxl = wli[IV1] - bm;
+    vxr = wri[IV1] - bp;
 
     fl[IDN] = wli[IDN] * vxl;
     fr[IDN] = wri[IDN] * vxr;
 
-    fl[IVX] = wli[IDN] * wli[IVX] * vxl + wli[IPR];
-    fr[IVX] = wri[IDN] * wri[IVX] * vxr + wri[IPR];
+    fl[IV1] = wli[IDN] * wli[IV1] * vxl + wli[IPR];
+    fr[IV1] = wri[IDN] * wri[IV1] * vxr + wri[IPR];
 
-    fl[IVY] = wli[IDN] * wli[IVY] * vxl;
-    fr[IVY] = wri[IDN] * wri[IVY] * vxr;
+    fl[IV2] = wli[IDN] * wli[IV2] * vxl;
+    fr[IV2] = wri[IDN] * wri[IV2] * vxr;
 
-    fl[IVZ] = wli[IDN] * wli[IVZ] * vxl;
-    fr[IVZ] = wri[IDN] * wri[IVZ] * vxr;
+    fl[IV3] = wli[IDN] * wli[IV3] * vxl;
+    fr[IV3] = wri[IDN] * wri[IV3] * vxr;
 
-    fl[IEN] = el * vxl + wli[IPR] * wli[IVX];
-    fr[IEN] = er * vxr + wri[IPR] * wri[IVX];
+    fl[IEN] = el * vxl + wli[IPR] * wli[IV1];
+    fr[IEN] = er * vxr + wri[IPR] * wri[IV1];
 
     //--- Step 8. Compute flux weights or scales
 
@@ -169,16 +145,18 @@ void Hydro::RiemannSolver(const int k, const int j, const int il, const int iu,
     // of the flux along the contact
 
     flxi[IDN] = sl * fl[IDN] + sr * fr[IDN];
-    flxi[IVX] = sl * fl[IVX] + sr * fr[IVX] + sm * cp;
-    flxi[IVY] = sl * fl[IVY] + sr * fr[IVY];
-    flxi[IVZ] = sl * fl[IVZ] + sr * fr[IVZ];
+    flxi[IV1] = sl * fl[IV1] + sr * fr[IV1] + sm * cp;
+    flxi[IV2] = sl * fl[IV2] + sr * fr[IV2];
+    flxi[IV3] = sl * fl[IV3] + sr * fr[IV3];
     flxi[IEN] = sl * fl[IEN] + sr * fr[IEN] + sm * cp * am;
 
-    flx(IDN, k, j, i) = flxi[IDN];
-    flx(ivx, k, j, i) = flxi[IVX];
-    flx(ivy, k, j, i) = flxi[IVY];
-    flx(ivz, k, j, i) = flxi[IVZ];
-    flx(IEN, k, j, i) = flxi[IEN];
+    cons.flux(ivx, IDN, k, j, i) = flxi[IDN];
+    cons.flux(ivx, ivx, k, j, i) = flxi[IV1];
+    cons.flux(ivx, ivy, k, j, i) = flxi[IV2];
+    cons.flux(ivx, ivz, k, j, i) = flxi[IV3];
+    cons.flux(ivx, IEN, k, j, i) = flxi[IEN];
+  });
   }
-  return;
-}
+};
+
+#endif // RSOLVERS_HYDRO_HLLC_HPP_
