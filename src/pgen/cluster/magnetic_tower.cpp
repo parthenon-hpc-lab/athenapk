812698ed0a339c52d36fb0eea484f241ba21fc85
Forrest Glines <forrestglines@gmail.com>
2023-01-31 18:02:06 -0600

    Fixed magnetic tower density bug

--- src/pgen/cluster/magnetic_tower.cpp
+++ src/pgen/cluster/magnetic_tower.cpp
@@ -131,7 +131,7 @@ void MagneticTower::AddSrcTerm(parthenon::Real field_to_add, parthenon::Real mas
 
         // Add density
         const Real cell_delta_rho =
-            mt.DensityFromSimCart(coords.Xc<1>(i), coords.Xc<1>(i), coords.Xc<1>(i));
+            mt.DensityFromSimCart(coords.Xc<1>(i), coords.Xc<2>(j), coords.Xc<3>(k));
         AddDensityToConsAtFixedVelTemp(cell_delta_rho, cons, prim, eos, k, j, i);
       });
 }
