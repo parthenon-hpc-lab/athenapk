9b97ed3c090517aa646240d1853ed8d7001b605d
Ben Wibking <ben@wibking.com>
2023-03-28 12:31:00 -0400

    update ascent actions

---
+++ plot_profiles.py
@@ -0,0 +1,37 @@
+import yaml
+import numpy as np
+import matplotlib.pyplot as plt
+
+with open("ascent_session.yaml",'r') as file:
+    session = yaml.load(file, Loader=yaml.FullLoader)
+
+def plot_profile(name):
+    print(f"plotting {name}...")
+    series = session[name]
+    profiles = []
+    times = []
+    bin_centers = []
+
+    for cycle in series.values():
+        profiles.append(cycle['attrs']['value']['value'])
+        times.append(cycle['time'])
+        bins = np.linspace( -106.25, 106.250002125, 64+1 )
+        bin_centers.append( 0.5*(bins[:-1] + bins[1:]) )
+
+    plt.figure(figsize=(6,4))
+    for i in range(0, len(profiles), 10):
+        plt.plot(bin_centers[i], profiles[i], label=times[i])
+
+    #plt.legend(loc='best')
+    plt.xlabel("z height (kpc)")
+    plt.ylabel(f"{name}")
+    plt.tight_layout()
+    plt.savefig(name + ".pdf")
+
+plot_profile("drho_profile")
+plot_profile("dK_profile")
+plot_profile("dP_profile")
+
+plot_profile("drho_rms_profile")
+plot_profile("dP_rms_profile")
+plot_profile("dK_rms_profile")
