ea2c6d47a046144b13ee0e258e9137ea13b1ba51
Ben Wibking <ben@wibking.com>
2023-05-18 17:00:40 -0400

    add input files

---
+++ plot_ascent_profiles.py
@@ -0,0 +1,46 @@
+import yaml
+import numpy as np
+import matplotlib.pyplot as plt
+
+def plot_profile(name, session=None):
+    data = session[name]
+    profiles = []
+    times = []
+    bin_centers = []
+
+    # loop over output times
+    for cycle in data.values():
+        # compute bins
+        bin_props = cycle['attrs']['bin_axes']['value']['cell_radius']
+        xmin = bin_props['min_val']
+        xmax = bin_props['max_val']
+        nbins = bin_props['num_bins']
+        bins = np.linspace(xmin, xmax, nbins+1)
+        bin_centers.append(0.5*(bins[:-1] + bins[1:]))
+
+        # get profile
+        profiles.append(np.asarray(
+            cycle['attrs']['value']['value'], dtype=np.float64))
+        times.append(cycle['time'])
+
+    plt.figure(figsize=(6, 4))
+    for i in range(len(profiles)):
+        plt.plot(bin_centers[i], profiles[i],
+                 label=r"$t = {:.5f}$".format(times[i]))
+
+    plt.xlim(0, 0.5)
+    plt.legend(loc='upper right')
+    plt.ylabel(f"{name}")
+    plt.xlabel(r"radius")
+    plt.tight_layout()
+    plt.savefig(name + ".png")
+
+if __name__ == "__main__":
+    # plot profiles produced by Ascent DataBinning function
+    
+    with open("ascent_profiles.yaml", 'r') as file:
+        session = yaml.load(file, Loader=yaml.FullLoader)
+        plot_profile("density_profile", session=session)
+        plot_profile("pressure_profile", session=session)
+        plot_profile("vorticity_profile", session=session)
+
