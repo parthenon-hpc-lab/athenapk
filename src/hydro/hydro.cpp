f699b888133b639e24d215710aec1c6e37fe584a
par-hermes <par-hermes@automation.bot>
2023-07-24 15:02:32 +0000

    cpp-py-formatter

--- src/hydro/hydro.cpp
+++ src/hydro/hydro.cpp
@@ -961,8 +961,9 @@ TaskStatus CalculateFluxes(std::shared_ptr<MeshData<Real>> &md) {
 // (multiple calls) versus extra memory usage is.
 template <Fluid fluid>
 TaskStatus FirstOrderFluxCorrect(MeshData<Real> *u0_data, MeshData<Real> *u1_data,
-                                 const Real gam0_, const Real gam1_, const Real beta_dt_) {
-  //Work around for CUDA <=11.6
+                                 const Real gam0_, const Real gam1_,
+                                 const Real beta_dt_) {
+  // Work around for CUDA <=11.6
   const Real gam0 = gam0_;
   const Real gam1 = gam1_;
   const Real beta_dt = beta_dt_;
