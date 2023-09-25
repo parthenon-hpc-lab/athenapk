4ab883459579b6f47e701145204c495601538b66
Philipp Grete <gretephi@msu.edu>
2023-09-25 22:24:08 +0200

    Fix test cases and add success check

--- tst/regression/test_suites/aniso_therm_cond_gauss_conv/aniso_therm_cond_gauss_conv.py
+++ tst/regression/test_suites/aniso_therm_cond_gauss_conv/aniso_therm_cond_gauss_conv.py
@@ -26,12 +26,13 @@ import sys
 import os
 import itertools
 import utils.test_case
+from scipy.optimize import curve_fit
 
 # To prevent littering up imported folders with .pyc files or __pycache_ folder
 sys.dont_write_bytecode = True
 
 int_cfgs = ["unsplit", "rkl2"]
-res_cfgs = [128, 256]
+res_cfgs = [128, 256, 512]
 field_cfgs = ["none", "aligned", "angle", "perp"]
 tlim = 2.0
 
@@ -65,7 +66,6 @@ def get_B(field_cfg):
 
 class TestCase(utils.test_case.TestCaseAbs):
     def Prepare(self, parameters, step):
-
         assert parameters.num_ranks <= 4, "Use <= 4 ranks for diffusion test."
 
         res, field_cfg, int_cfg = all_cfgs[step - 1]
@@ -90,6 +90,8 @@ class TestCase(utils.test_case.TestCaseAbs):
             "parthenon/mesh/x2max=1.0",
             "parthenon/mesh/nx3=1",
             "parthenon/meshblock/nx3=1",
+            "parthenon/time/integrator=%s"
+            % ("rk2" if (int_cfg == "unsplit") else "rk1"),
             "problem/diffusion/Bx=%f" % Bx,
             "problem/diffusion/By=%f" % By,
             "problem/diffusion/iprob=10",
@@ -104,7 +106,6 @@ class TestCase(utils.test_case.TestCaseAbs):
         return parameters
 
     def Analyse(self, parameters):
-
         sys.path.insert(
             1,
             parameters.parthenon_path
@@ -117,12 +118,14 @@ class TestCase(utils.test_case.TestCaseAbs):
             print("Couldn't find module to read Parthenon hdf5 files.")
             return False
 
+        tests_passed = True
+
         def get_ref(x, Bx, field_cfg):
             eff_diff_coeff = 0.25 if Bx == 0.0 else 0.25 * Bx * Bx
             tlim_ = 0.0 if field_cfg == "perp" else tlim
             return 1.0 + 1e-6 / (
                 np.sqrt(4 * np.pi * eff_diff_coeff * (0.5 + tlim_))
-                / np.exp(-(x ** 2) / (4.0 * eff_diff_coeff * (0.5 + tlim_)))
+                / np.exp(-(x**2) / (4.0 * eff_diff_coeff * (0.5 + tlim_)))
             )
 
         num_rows = len(res_cfgs)
@@ -137,7 +140,7 @@ class TestCase(utils.test_case.TestCaseAbs):
             prim = data_file.Get("prim")
             zz, yy, xx = data_file.GetVolumeLocations()
             mask = yy == yy[0]
-            temp = prim[:, 4][mask]
+            temp = prim[4][mask]
             x = xx[mask]
             res, field_cfg, int_cfg = all_cfgs[step]
             row = res_cfgs.index(res)
@@ -151,24 +154,51 @@ class TestCase(utils.test_case.TestCaseAbs):
                 int_cfgs.index(int_cfg),
                 res_cfgs.index(res),
             ] = l1
-            p[row, col].plot(x, temp, label=field_cfg + " L$_1$=%.2g" % l1)
+            p[row, col].plot(x, temp, label=f"{field_cfg} N={res} L$_1$={l1:.2g}")
 
         # Plot convergence
         for i, field_cfg in enumerate(field_cfgs):
             for j, int_cfg in enumerate(int_cfgs):
+                p[0, j].set_title(f"Integrator: {int_cfg}")
                 if field_cfg == "perp":
                     continue
 
-                est_conv = np.diff(np.log(l1_err[i, j, :])) / np.diff(np.log(res_cfgs))
                 p[-1, j].plot(
                     res_cfgs,
                     l1_err[i, j, :],
-                    label=field_cfg + " conv: %.2f" % est_conv,
+                    label=f"{field_cfg} data",
                 )
+
+                # Simple convergence estimator
+                conv_model = lambda log_n, log_a, conv_rate: conv_rate * log_n + log_a
+                popt, pconv = curve_fit(
+                    conv_model, np.log(res_cfgs), np.log(l1_err[i, j, :])
+                )
+                conv_a, conv_measured = popt
+                # Note that the RKL2 convergence on the plots is currently significantly better
+                # than expected (<-3) though the L1 errors themself are larger than the unsplit
+                # integrator (as expected).
+                # For a more reasonable test (which would take longer), reduce the RKL2 ratio to,
+                # say, 200 and extend the resolution grid to 1024 (as the first data point at N=128
+                # is comparatively worse than at N>128).
+                if conv_measured > -1.98:
+                    print(
+                        f"!!!\nConvergence for {field_cfg} test with {int_cfg} integrator "
+                        f"is worse ({conv_measured}) than expected (-1.98).\n!!!"
+                    )
+                    tests_passed = False
+                p[-1, j].plot(
+                    res_cfgs,
+                    np.exp(conv_a) * res_cfgs**conv_measured,
+                    ":",
+                    lw=0.75,
+                    label=f"{field_cfg} Measured conv: {conv_measured:.2f}",
+                )
+
         p[-1, 0].set_xscale("log")
         p[-1, 0].set_yscale("log")
-        p[-1, 0].legend(fontsize=8)
-        p[-1, 1].legend(fontsize=8)
+        p[-1, 0].legend(fontsize=6)
+        p[-1, 1].legend(fontsize=6)
 
         # Plot reference lines
         x = np.linspace(-6, 6, 400)
@@ -179,11 +209,13 @@ class TestCase(utils.test_case.TestCaseAbs):
                     y = get_ref(x, Bx, field_cfg)
                     p[i, j].plot(x, y, "-", lw=0.5, color="black", alpha=0.8)
                     p[i, j].grid()
-                    p[i, j].legend(fontsize=8)
+                    p[i, j].legend(fontsize=6)
 
         fig.tight_layout()
         fig.savefig(
-            os.path.join(parameters.output_path, "cond.png"), bbox_inches="tight"
+            os.path.join(parameters.output_path, "cond.png"),
+            bbox_inches="tight",
+            dpi=300,
         )
 
-        return True
+        return tests_passed
