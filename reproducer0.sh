6790d001914d44880ca6cd3c71381bce18b618e7
Ben Wibking <ben@wibking.com>
2024-10-27 16:56:56 -0400

    add new reproducer scripts

---
+++ reproducer0.sh
@@ -0,0 +1,30 @@
+#!/bin/sh
+
+set -x
+
+# compile
+mkdir build
+cd build
+cmake .. -DPARTHENON_ENABLE_PYTHON_MODULE_CHECK=OFF
+cmake --build .
+cd ..
+
+# first run
+mpirun -np 8 ./build/bin/athenaPK -i inputs/precipitator_restart_reproducer.in
+
+# move outputs to avoid being overwritten
+mkdir first_run
+mv parthenon.* first_run
+rm *.csv
+
+# restart run from time zero
+mpirun -np 8 ./build/bin/athenaPK -r first_run/parthenon.restart.00000.rhdf
+
+# clean up
+rm *.csv
+
+# compare SECOND restart outputs
+h5diff first_run/parthenon.restart.00002.rhdf parthenon.restart.00002.rhdf
+
+# compare with phdf_diff
+uv run external/parthenon/scripts/python/packages/parthenon_tools/parthenon_tools/phdf_diff.py -one parthenon.restart.00002.rhdf first_run/parthenon.restart.00002.rhdf
--- reproducer1.sh
+++ reproducer1.sh
@@ -17,7 +17,7 @@ mkdir first_run
 mv parthenon.* first_run
 rm *.csv
 
-# restart run
+# restart run from SECOND restart file
 mpirun -np 8 ./build/bin/athenaPK -r first_run/parthenon.restart.00001.rhdf
 
 # clean up
@@ -26,8 +26,5 @@ rm *.csv
 # compare restart outputs
 h5diff first_run/parthenon.restart.00002.rhdf parthenon.restart.00002.rhdf
 
-# compare snapshot outputs
-h5diff first_run/parthenon.prim.00002.phdf parthenon.prim.00002.phdf
-
 # compare with phdf_diff
-python3 external/parthenon/scripts/python/packages/parthenon_tools/parthenon_tools/phdf_diff.py -brief parthenon.restart.00002.rhdf first_run/parthenon.restart.00002.rhdf
+uv run external/parthenon/scripts/python/packages/parthenon_tools/parthenon_tools/phdf_diff.py -one parthenon.restart.00002.rhdf first_run/parthenon.restart.00002.rhdf
