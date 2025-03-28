e6a56d67b652eb5bc0d1c2792d79137190d9cf01
Philipp Grete <pgrete@hs.uni-hamburg.de>
2025-03-28 11:22:50 +0100

    Update particle seeding logic

--- src/tracers/tracers.cpp
+++ src/tracers/tracers.cpp
@@ -47,18 +47,6 @@ std::shared_ptr<StateDescriptor> Initialize(ParameterInput *pin) {
 
   Params &params = tracer_pkg->AllParams();
 
-  const auto num_tracers_per_cell =
-      pin->GetOrAddReal("tracers", "num_tracers_per_cell", 0.0);
-  PARTHENON_REQUIRE_THROWS(num_tracers_per_cell >= 0.0,
-                           "What's a negative number of particles per cell?!");
-  params.Add("num_tracers_per_cell", num_tracers_per_cell);
-
-  // Initialize random number generator pool
-  int rng_seed = pin->GetOrAddInteger("tracers", "rng_seed", time(NULL));
-  tracer_pkg->AddParam<>("rng_seed", rng_seed);
-  RNGPool rng_pool(rng_seed);
-  tracer_pkg->AddParam<>("rng_pool", rng_pool);
-
   // Add swarm of tracers
   std::string swarm_name = "tracers";
   // TODO(pgrete) Check where metadata, e.g., for restart is required (i.e., at the swarm
@@ -122,58 +110,74 @@ void SeedInitialTracers(Mesh *pmesh, ParameterInput *pin, parthenon::SimTime &tm
 
   auto tracers_pkg = pmesh->packages.Get("tracers");
 
-  for (auto &pmb : pmesh->block_list) {
-    auto &swarm = pmb->meshblock_data.Get()->GetSwarmData()->Get("tracers");
-    auto rng_pool = tracers_pkg->Param<RNGPool>("rng_pool");
-
-    IndexRange ib = pmb->cellbounds.GetBoundsI(IndexDomain::interior);
-    IndexRange jb = pmb->cellbounds.GetBoundsJ(IndexDomain::interior);
-    IndexRange kb = pmb->cellbounds.GetBoundsK(IndexDomain::interior);
-
-    const Real &x_min = pmb->coords.Xf<1>(ib.s);
-    const Real &y_min = pmb->coords.Xf<2>(jb.s);
-    const Real &z_min = pmb->coords.Xf<3>(kb.s);
-    const Real &x_max = pmb->coords.Xf<1>(ib.e + 1);
-    const Real &y_max = pmb->coords.Xf<2>(jb.e + 1);
-    const Real &z_max = pmb->coords.Xf<3>(kb.e + 1);
-
-    // as for num_tracers on each block... will get too many on multiple blocks
-    // TODO: distribute amongst blocks.
-    const auto num_tracers_per_cell = tracers_pkg->Param<Real>("num_tracers_per_cell");
+  const auto seed_method = pin->GetOrAddString("tracers", "initial_seed_method", "none");
+  if (seed_method == "none") {
+    return;
+  } else if (seed_method == "random_per_block") {
+    const auto num_tracers_per_cell =
+        pin->GetOrAddReal("tracers", "initial_num_tracers_per_cell", 0.0);
+    PARTHENON_REQUIRE_THROWS(num_tracers_per_cell > 0.0,
+                             "You should seed at least some tracers.");
     const auto num_tracers_per_block =
-        static_cast<int>(pmb->GetNumberOfMeshBlockCells() * num_tracers_per_cell);
+        static_cast<int>(pmesh->GetNumberOfMeshBlockCells() * num_tracers_per_cell);
+    PARTHENON_REQUIRE_THROWS(num_tracers_per_block > 0,
+                             "Resulting number of particles per block is invalid.");
 
-    // Create new particles and get accessor
-    auto new_particles_context = swarm->AddEmptyParticles(num_tracers_per_block);
+    // Initialize random number generator pool
+    int rng_seed = pin->GetOrAddInteger("tracers", "initial_rng_seed", 0);
 
-    auto &x = swarm->Get<Real>(swarm_position::x::name()).Get();
-    auto &y = swarm->Get<Real>(swarm_position::y::name()).Get();
-    auto &z = swarm->Get<Real>(swarm_position::z::name()).Get();
-    auto &id = swarm->Get<int>("id").Get();
+    for (auto &pmb : pmesh->block_list) {
+      auto &swarm = pmb->meshblock_data.Get()->GetSwarmData()->Get("tracers");
+      // Seed is meshblock gid for consistency across MPI decomposition
+      RNGPool rng_pool(pmb->gid + rng_seed);
 
-    auto swarm_d = swarm->GetDeviceContext();
+      IndexRange ib = pmb->cellbounds.GetBoundsI(IndexDomain::interior);
+      IndexRange jb = pmb->cellbounds.GetBoundsJ(IndexDomain::interior);
+      IndexRange kb = pmb->cellbounds.GetBoundsK(IndexDomain::interior);
 
-    const int gid = pmb->gid;
-    pmb->par_for(
-        "ProblemGenerator::Turbulence::DistributeTracers", 0,
-        new_particles_context.GetNewParticlesMaxIndex(), KOKKOS_LAMBDA(const int new_n) {
-          auto rng_gen = rng_pool.get_state();
-          const int n = new_particles_context.GetNewParticleIndex(new_n);
+      const auto &x_min = pmb->coords.Xf<1>(ib.s);
+      const auto &y_min = pmb->coords.Xf<2>(jb.s);
+      const auto &z_min = pmb->coords.Xf<3>(kb.s);
+      const auto &x_max = pmb->coords.Xf<1>(ib.e + 1);
+      const auto &y_max = pmb->coords.Xf<2>(jb.e + 1);
+      const auto &z_max = pmb->coords.Xf<3>(kb.e + 1);
 
-          x(n) = x_min + rng_gen.drand() * (x_max - x_min);
-          y(n) = y_min + rng_gen.drand() * (y_max - y_min);
-          z(n) = z_min + rng_gen.drand() * (z_max - z_min);
-          // Note that his works only during one time init.
-          // If (somehwere else) we eventually add dynamic particles, then we need to
-          // manage ids (not indices) more globally.
-          id(n) = num_tracers_per_block * gid + n;
+      // Create new particles and get accessor
+      auto new_particles_context = swarm->AddEmptyParticles(num_tracers_per_block);
 
-          rng_pool.free_state(rng_gen);
+      auto &x = swarm->Get<Real>(swarm_position::x::name()).Get();
+      auto &y = swarm->Get<Real>(swarm_position::y::name()).Get();
+      auto &z = swarm->Get<Real>(swarm_position::z::name()).Get();
+      auto &id = swarm->Get<int>("id").Get();
 
-          // TODO(pgrete) check if this actually required
-          bool on_current_mesh_block = true;
-          swarm_d.GetNeighborBlockIndex(n, x(n), y(n), z(n), on_current_mesh_block);
-        });
+      auto swarm_d = swarm->GetDeviceContext();
+
+      const auto gid = pmb->gid;
+      pmb->par_for(
+          "ProblemGenerator::Turbulence::DistributeTracers", 0,
+          new_particles_context.GetNewParticlesMaxIndex(),
+          KOKKOS_LAMBDA(const int new_n) {
+            auto rng_gen = rng_pool.get_state();
+            const int n = new_particles_context.GetNewParticleIndex(new_n);
+
+            x(n) = x_min + rng_gen.drand() * (x_max - x_min);
+            y(n) = y_min + rng_gen.drand() * (y_max - y_min);
+            z(n) = z_min + rng_gen.drand() * (z_max - z_min);
+            // Note that his works only during one time init.
+            // If (somehwere else) we eventually add dynamic particles, then we need to
+            // manage ids (not indices) more globally.
+            id(n) = num_tracers_per_block * gid + n;
+
+            rng_pool.free_state(rng_gen);
+
+            // TODO(pgrete) check if this actually required
+            bool on_current_mesh_block = true;
+            swarm_d.GetNeighborBlockIndex(n, x(n), y(n), z(n), on_current_mesh_block);
+          });
+    }
+  } else if (seed_method == "user") {
+  } else {
+    PARTHENON_THROW("Unknown tracer initial_seed_method");
   }
 }
 
