79b0b8a161b388572d51039aa92419c6d35f9a01
mfournier01 <142504260+mfournier01@users.noreply.github.com>
2025-07-10 12:01:54 +0200

    Update particle_advection_vinterp.py

--- tst/regression/test_suites/particle_advection_vinterp/particle_advection_vinterp.py
+++ tst/regression/test_suites/particle_advection_vinterp/particle_advection_vinterp.py
@@ -35,7 +35,7 @@ class TestCase(utils.test_case.TestCaseAbs):
             "parthenon/meshblock/nx2=16",
             "parthenon/meshblock/nx3=8",
             "tracers/enabled=true",
-            "tracers/method=vinterp",
+            "tracers/advection_method=vinterp",
             "tracers/swarm_names=tracers0",
             "tracers/initial_seed_method=random_per_block",
             "tracers/tracers0/initial_num_tracers_per_cell=0.125",
