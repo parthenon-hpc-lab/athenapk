078cf4afd8a9333ada8588b286bbd6482dd1d173
Philipp Grete <pgrete@hs.uni-hamburg.de>
2025-08-28 11:03:18 +0200

    Update/backport n_lookback logic (untested)

--- tst/regression/test_suites/diffusion/diffusion.py
+++ tst/regression/test_suites/diffusion/diffusion.py
@@ -1,4 +1,4 @@
-========================================================================================
+# ========================================================================================
 # AthenaPK - a performance portable block structured AMR MHD code
 # Copyright (c) 2023-2024, Athena Parthenon Collaboration. All rights reserved.
 # Licensed under the 3-clause BSD License, see LICENSE file for details
