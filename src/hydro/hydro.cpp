ffe82f578eb2306b377104dccae6df5033e86424
fournier2 <fournier2@jwlogin24.juwels>
2025-10-20 12:15:34 +0200

    monte carlo

--- src/hydro/hydro.cpp
+++ src/hydro/hydro.cpp
@@ -1073,6 +1073,28 @@ TaskStatus CalculateFluxes(std::shared_ptr<MeshData<Real>> &md) {
     fvel = md->PackVariables(std::vector<std::string>{"fvel"});
   }
 
+  // If Monte-Carlo based advection, need to save the initial mass of each cell
+  // before its value is being updated by the hydro solver.
+  auto M_cell = parthenon::MeshBlockPack<parthenon::VariablePack<parthenon::Real>>{};
+  if (advection_method == AdvectMethod::MonteCarlo) {
+    M_cell = md->PackVariables(std::vector<std::string>{"M_cell"});
+
+    parthenon::par_for(
+        DEFAULT_LOOP_PATTERN, "Fill Mcell", parthenon::DevExecSpace(), 0,
+        cons_in.GetDim(5) - 1, kb.s, kb.e, jb.s, jb.e, ib.s, ib.e,
+        KOKKOS_LAMBDA(const int b, const int k, const int j, const int i) {
+          const auto &cons = cons_in(b);
+          const auto &coords = cons_in.GetCoords(b);
+          auto &M = M_cell(b);
+
+          // Get the cell volume from device-accessible view
+          const Real dV = coords.Volume<TE::CC>(k, j, i);
+
+          // Store total cell mass
+          M(0, k, j, i) = cons(IDN, k, j, i) * dV;
+        });
+  }
+
   // Loading equation of state
   const auto &eos =
       pkg->Param<typename std::conditional<fluid == Fluid::euler, AdiabaticHydroEOS,
@@ -1313,9 +1335,9 @@ TaskStatus FirstOrderFluxCorrect(MeshData<Real> *u0_data, MeshData<Real> *u1_dat
 
   std::int64_t num_corrected, num_need_floor;
   // Potentially need multiple attempts as flux correction corrects 6 (in 3D) fluxes
-  // of a single cell at the same time. So the neighboring cells need to be rechecked with
-  // the corrected fluxes as the corrected fluxes in one cell may result in the need to
-  // correct all the fluxes of an originally "good" neighboring cell.
+  // of a single cell at the same time. So the neighboring cells need to be rechecked
+  // with the corrected fluxes as the corrected fluxes in one cell may result in the
+  // need to correct all the fluxes of an originally "good" neighboring cell.
   size_t num_attempts = 0;
   do {
     num_corrected = 0;
